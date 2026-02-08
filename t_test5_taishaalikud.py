e2c0092b18d79071eb423a6a9ffa442a4e1f09a1
martin76-hub <martin76@tlu.ee>
2026-02-08 15:55:07 +0000

    Your commit message here

--- t_test5_taishaalikud.py
+++ t_test5_taishaalikud.py
@@ -23,3 +23,22 @@ def keskmine(m):
     return sum(m)/len(m)
 
 print(keskmine(arvud1), keskmine(arvud2))
+
+def t_osa(sona):
+    return len([t for t in sona if t in th])/len(sona)
+
+osakaalud1=[t_osa(sona) for sona in lause1.lower().split()]
+print(osakaalud1)
+print(keskmine(osakaalud1))
+
+osakaalud2=[t_osa(sona) for sona in lause2.lower().split()]
+print(osakaalud2)
+print(keskmine(osakaalud2))
+
+print(keskmine(osakaalud1), keskmine(osakaalud2))
+
+print(ttest_ind(osakaalud1, osakaalud2))
+
+import statsmodels.stats.api as sms
+cm = sms.CompareMeans(sms.DescrStatsW(osakaalud1), sms.DescrStatsW(osakaalud2))
+print(cm.tconfig_diff(usevar='unequal'))
